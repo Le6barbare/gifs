9514f37b4dea19ff5df65c3db0296bf5a3984e9a
Le6barbare <Le6barbare@gmail.com>
2016-03-12 01:56:33 +0100

    first try

---
+++ index.md
@@ -0,0 +1,8 @@
+---
+layout: home
+---
+
+# My Gif Collection
+
+<p>Collection of gifs. <br>Original code by <a href="https://github.com/jglovier/gifs/">jglovier/gifs</a>, my fork is at <a href="https://github.com/le6barbare/gifs/">le6barbare/gifs</a> <br> (Hover to preview an image)</p>
+<p><a href="library.gifwit">Download the library for gifwit</a></p>
---
+++ script/build.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.3
+
+import os
+
+Path="img/"
+
+ListDirectory = os.listdir(Path)
+
+with open('__includes/site-index.html', 'w') as OpenFile:
+    OpenFile.write('<ul>')
+    for Dir in ListDirectory:
+        OpenFile.write('<li>'+Dir+'<ul>')
+        ListFile = os.listdir(Path+Dir)
+        for File in ListFile:
+            OpenFile.write('<li><a href="./'+Path+Dir+'/'+File+'" class="gif">'+File+'</a></li>')
+        OpenFile.write('</ul></li>')
+    OpenFile.write('</ul></div>')
