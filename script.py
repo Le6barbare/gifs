cca6cdc6afb63c70ee2d3394589926ecb075ea3b
Le6barbare <Le6barbare@gmail.com>
2016-03-12 02:42:46 +0100

    :boom: ??

--- script.py
+++ script.py
@@ -7,11 +7,16 @@ Path="img/"
 ListDirectory = os.listdir(Path)
 
 with open('__includes/site-index.html', 'w') as OpenFile:
-    OpenFile.write('<ul>')
-    for Dir in ListDirectory:
-        OpenFile.write('<li>'+Dir+'<ul>')
-        ListFile = os.listdir(Path+Dir)
-        for File in ListFile:
-            OpenFile.write('<li><a href="./'+Path+Dir+'/'+File+'" class="gif">'+File+'</a></li>')
-        OpenFile.write('</ul></li>')
-    OpenFile.write('</ul>')
+    with open('__data/static_files.yml', 'w') as StaticFile:
+        OpenFile.write('<ul>')
+        StaticFile.write('---\n')
+        for Dir in ListDirectory:
+            OpenFile.write('<li>'+Dir+'<ul>')
+            ListFile = os.listdir(Path+Dir)
+            for File in ListFile:
+                OpenFile.write('<li><a href="./'+Path+Dir+'/'+File+'" class="gif">'+File+'</a></li>')
+                StaticFile.write('- path: '+Path+Dir+'/'+File+'\n')
+                StaticFile.write('  modified_time: 1430463202\n')
+                StaticFile.write('  extname: "'+os.path.splitext(File)[1]+'"\n')
+            OpenFile.write('</ul></li>')
+        OpenFile.write('</ul>')
